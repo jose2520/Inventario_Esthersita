f63c3fca673a750231198f01c94d555373bf660e
jose del carmen diaz diaz <josedelcarmen_diaz@outlook.com>
2026-03-30 19:16:43 -0500

    se actualizo el archivo inventario: se agrego diseño , input y muestra

--- inventario.py
+++ inventario.py
@@ -1,3 +1,46 @@
-nombre = input("Ingresar el nombre del producto")
-precio = float(input("Ingresar el precio del producto"))
-cantidad = int(input("Ingresa el Stock del producto"))
+import sys
+import time
+
+# Colores
+VERDE = "\033[32m"
+AZUL = "\033[94m"
+ROJO = "\033[91m"
+AMARILLO = "\033[93m"
+CYAN = "\033[36m"
+RESET = "\033[0m"
+
+
+# --- ENTRADA DINÁMICA ---
+
+# 1. Animación del Título Principal
+titulo = " REGISTRO DE PRODUCTO "
+borde_t = f"+{'-' * len(titulo)}+"
+print(f"{VERDE}{borde_t}")
+for char in f"|{titulo}|":
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.02)
+print(f"\n{borde_t}{RESET}")
+
+# 2. Inputs Para Solicitar datos al Usuario..
+nombre = input(f"{CYAN} | Nombre del producto | -> {RESET}")  
+precio = float(input(f"{CYAN} | Precio unitario ($) | -> {RESET}"))
+cantidad = int(input(f"{CYAN} | Stock disponible    | -> {RESET}"))
+
+# Cálculo
+costo_total = precio * cantidad
+
+# --- MOSTRAR RESULTADO ---
+# Creamos la línea de datos
+datos = f" Producto: {nombre} | Precio: ${precio:.2f} | Stock: {cantidad} | Total: ${costo_total:.2f} "
+borde_f = "-" * len(datos)
+
+print(f"\n{VERDE}{borde_f}")
+for char in f"|{datos}|":
+    sys.stdout.write(char)
+    sys.stdout.flush()
+    time.sleep(0.02)
+print(f"\n{borde_f}{RESET}\n")
+
+
+
